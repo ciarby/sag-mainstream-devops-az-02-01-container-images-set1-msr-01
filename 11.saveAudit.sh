cb45e8333dc432786b8e439c6c58bda4eab764ae
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-07-05 11:41:09 +0200

    Fix: old vars in 11.saveAudit.sh

--- 11.saveAudit.sh
+++ 11.saveAudit.sh
@@ -4,5 +4,5 @@
 . "${SUIF_CACHE_HOME}/01.scripts/commonFunctions.sh"
 
 logI "Saving the audit"
-mkdir -p "$MY_sd/sessions/$MY_crtDay"
-tar cvzf "$MY_sd/sessions/$MY_crtDay/s_$MY_d.tgz" "${SUIF_AUDIT_BASE_DIR}"
+mkdir -p "$MY_sd/sessions/$JOB_DATE"
+tar cvzf "$MY_sd/sessions/$JOB_DATE/s_$JOB_DATETIME.tgz" "${SUIF_AUDIT_BASE_DIR}"
